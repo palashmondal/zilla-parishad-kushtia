f6f97f03945bfbc7b291d657dacdb4d9ae7ef0c8
Palash Mondal <5854890+palashmondal@users.noreply.github.com>
2026-04-23 03:41:49 +0600

    bug fixed

--- database/migrations/20260420_approval_memos_unique_memo_number.sql
+++
@@ -1,5 +0,0 @@
--- Ensure memo_number is unique in approval_memos.
--- Safe to run on DBs that already have the index (migrate.js ignores "already exists" errors).
-
-ALTER TABLE `approval_memos`
-  ADD UNIQUE INDEX `idx_memo_number_unique` (`memo_number`);
